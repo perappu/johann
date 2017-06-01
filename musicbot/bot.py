18c62a4589aadfdb82ce561e03b875c7fbf02a09
Stokori <stokoriel@gmail.com>
2017-06-01 02:21:44 -0500

    Change how autoplaylist works

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -77,6 +77,9 @@ class MusicBot(discord.Client):
 
         self.blacklist = set(load_file(self.config.blacklist_file))
         self.autoplaylist = load_file(self.config.auto_playlist_file)
+        #So that it doesn't replay the same song 5000 times in one night
+        shuffle(self.autoplaylist)
+        self.autoplaylist_counter = 0
         self.downloader = downloader.Downloader(download_folder='audio_cache')
 
         self.exit_signal = None
@@ -416,7 +419,11 @@ class MusicBot(discord.Client):
     async def on_player_finished_playing(self, player, **_):
         if not player.playlist.entries and not player.current_entry and self.config.auto_playlist:
             while self.autoplaylist:
-                song_url = choice(self.autoplaylist)
+                if(len(self.autoplaylist) <= self.autoplaylist_counter):
+                    self.autoplaylist_counter = 0
+                    shuffle(self.autoplaylist)
+                song_url = self.autoplaylist[self.autoplaylist_counter]
+                autoplaylist_counter += 1
                 info = await self.downloader.safe_extract_info(player.playlist.loop, song_url, download=False, process=False)
 
                 if not info:
